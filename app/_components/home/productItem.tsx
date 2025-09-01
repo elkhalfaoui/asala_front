d09a806a66e2c80998b313d1e958b62f3e2fc6c4
zakaria <elkhalfaouizakaria@gmail.com>
2025-09-01 18:20:00 +0200

    fixing the product item

--- app/_components/home/productItem.tsx
+++ app/_components/home/productItem.tsx
@@ -8,36 +8,32 @@ const ProductItem = ({ product }: { product: SimpleProduct }) => {
     <li>
       <Link
         href={`/products/${product.id}`}
-        className="flex flex-col justify-between items-center h-full"
+        className="flex flex-col gap-2 w-full"
       >
-        {/* Image container */}
-        <div className="w-full aspect-square overflow-hidden rounded-sm relative group">
-          <Image
-            src={`${product.mainImage}`}
-            alt="image"
-            width={400}
-            height={400}
-            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
-          />
+        <Image
+          src={`${product.mainImage}`}
+          alt="image"
+          width={400}
+          height={400}
+          className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
+        />
 
-          {/* Overlay with content */}
-          <div className="absolute inset-0 flex flex-col justify-end p-3 bg-gradient-to-t from-black/70 via-black/40 to-transparent text-white">
-            <h3 className="text-lg font-semibold">{product.title}</h3>
-            <div className="flex gap-0.5 text-yellow-400 mt-1">
-              {Array.from(
-                { length: Math.floor(product.averageRating) },
-                (_, i) => (
-                  <Star key={i} size={16} />
-                )
-              )}
-              {product.averageRating % 1 >= 0.4 && <StarHalf size={16} />}
-            </div>
-            <h4 className="mt-1">
-              <span className="text-sm text-zinc-300">starting from</span>{" "}
-              {product.startingPrice}
-              DH
-            </h4>
+        <div className="flex flex-col justify-end p-3">
+          <h3 className="text-lg font-semibold">{product.title}</h3>
+          <div className="flex gap-0.5 text-yellow-400 mt-1">
+            {Array.from(
+              { length: Math.floor(product.averageRating) },
+              (_, i) => (
+                <Star key={i} size={16} />
+              )
+            )}
+            {product.averageRating % 1 >= 0.4 && <StarHalf size={16} />}
           </div>
+          <h4 className="mt-1">
+            <span className="text-sm text-zinc-300">starting from</span>{" "}
+            {product.startingPrice}
+            DH
+          </h4>
         </div>
       </Link>
     </li>
